4e191c79157849832b44a94a954b6aa91a3bf174
leocorne <leonardocornelatti@gmail.com>
2020-05-28 16:49:34 +0200

    Changed to new Arduino library
    updated TF code

--- arduino_command_responder.cpp
+++ arduino_command_responder.cpp
@@ -13,95 +13,69 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
+#if defined(ARDUINO) && !defined(ARDUINO_ARDUINO_NANO33BLE)
+#define ARDUINO_EXCLUDE_CODE
+#endif  // defined(ARDUINO) && !defined(ARDUINO_ARDUINO_NANO33BLE)
+
+#ifndef ARDUINO_EXCLUDE_CODE
+
 #include "command_responder.h"
 
 #include "Arduino.h"
 
-// Use the RGB LED
-#include "PinNames.h"
-const PinName LED_GREEN = PinName::p16;
-const PinName LED_RED = PinName::p24;
-const PinName LED_BLUE = PinName::p6;
-const unsigned int DELAY = 1000/6;
-
-// Toggles the LED every inference, and keeps it on for ~2 seconds if a "yes"
-// was heard
+// Toggles the built-in LED every inference, and lights a colored LED depending
+// on which word was detected.
 void RespondToCommand(tflite::ErrorReporter* error_reporter,
                       int32_t current_time, const char* found_command,
                       uint8_t score, bool is_new_command) {
   static bool is_initialized = false;
   if (!is_initialized) {
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(LED_RED, OUTPUT);
-    pinMode(LED_GREEN, OUTPUT);
-    pinMode(LED_BLUE, OUTPUT);
-
-    digitalWrite(LED_GREEN, HIGH);
-    digitalWrite(LED_RED, HIGH);
-    digitalWrite(LED_BLUE, HIGH);
+    // Pins for the built-in RGB LEDs on the Arduino Nano 33 BLE Sense
+    pinMode(LEDR, OUTPUT);
+    pinMode(LEDG, OUTPUT);
+    pinMode(LEDB, OUTPUT);
+    // Ensure the LED is off by default.
+    // Note: The RGB LEDs on the Arduino Nano 33 BLE
+    // Sense are on when the pin is LOW, off when HIGH.
+    digitalWrite(LEDR, HIGH);
+    digitalWrite(LEDG, HIGH);
+    digitalWrite(LEDB, HIGH);
     is_initialized = true;
   }
-  static int32_t last_yes_time = 0;
-  static int32_t last_no_time = 0;
-  static int32_t last_uk_time = 0;
+  static int32_t last_command_time = 0;
   static int count = 0;
+  static int certainty = 220;
 
   if (is_new_command) {
-    error_reporter->Report("Heard %s (%d) @%dms", found_command, score,
-                           current_time);
-    // If we heard a "yes" or "no", switch on an LED and store the time.
+    TF_LITE_REPORT_ERROR(error_reporter, "Heard %s (%d) @%dms", found_command,
+                         score, current_time);
+    // If we hear a command, light up the appropriate LED
     if (found_command[0] == 'y') {
-      last_yes_time = current_time;
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_GREEN, LOW);
+      last_command_time = current_time;
+      digitalWrite(LEDG, LOW);  // Green for yes
     }
+
     if (found_command[0] == 'n') {
-      last_no_time = current_time;
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_RED, LOW);
+      last_command_time = current_time;
+      digitalWrite(LEDR, LOW);  // Red for no
     }
+
     if (found_command[0] == 'u') {
-      last_uk_time = current_time;
-      digitalWrite(LED_BUILTIN, HIGH);
-      digitalWrite(LED_RED, LOW);
-      digitalWrite(LED_BLUE, LOW);
-      digitalWrite(LED_GREEN, LOW);
+      last_command_time = current_time;
+      digitalWrite(LEDB, LOW);  // Blue for unknown
     }
   }
 
-  // If last_yes_time is non-zero but was >3 seconds ago, zero it
+  // If last_command_time is non-zero but was >3 seconds ago, zero it
   // and switch off the LED.
-  if (last_yes_time != 0) {
-    if (last_yes_time < (current_time - 3000)) {
-      last_yes_time = 0;
+  if (last_command_time != 0) {
+    if (last_command_time < (current_time - 3000)) {
+      last_command_time = 0;
       digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(LED_GREEN, HIGH);
-    }
-    // If it is non-zero but <3 seconds ago, do nothing.
-    return;
-  }
-
-  // If last_no_time is non-zero but was >3 seconds ago, zero it
-  // and switch off the LED.
-  if (last_no_time != 0) {
-    if (last_no_time < (current_time - 3000)) {
-      last_no_time = 0;
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(LED_RED, HIGH);
-    }
-    // If it is non-zero but <3 seconds ago, do nothing.
-    return;
-  }
-
-  // If last_no_time is non-zero but was >3 seconds ago, zero it
-  // and switch off the LED.
-  if (last_uk_time != 0) {
-    if (last_uk_time < (current_time - 3000)) {
-      last_uk_time = 0;
-      digitalWrite(LED_BUILTIN, LOW);
-      digitalWrite(LED_RED, HIGH);
-      digitalWrite(LED_GREEN, HIGH);
-      digitalWrite(LED_BLUE, HIGH);
+      digitalWrite(LEDR, HIGH);
+      digitalWrite(LEDG, HIGH);
+      digitalWrite(LEDB, HIGH);
     }
     // If it is non-zero but <3 seconds ago, do nothing.
     return;
@@ -115,3 +89,5 @@ void RespondToCommand(tflite::ErrorReporter* error_reporter,
     digitalWrite(LED_BUILTIN, LOW);
   }
 }
+
+#endif  // ARDUINO_EXCLUDE_CODE
